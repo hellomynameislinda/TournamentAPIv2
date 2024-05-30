e0fd90e89547816332d5c5ed845115f8e079ef4a
Linda <linda@hellomynameislinda.com>
2024-05-30 20:27:52 +0200

    Updated with message in program.cs

--- TournamentAPIv2/Program.cs
+++ TournamentAPIv2/Program.cs
@@ -5,6 +5,13 @@ using TournamentAPIv2.Core.Repositories;
 using TournamentAPIv2.Data.Data;
 using TournamentAPIv2.Data.Repositories;
 
+/*
+1. Jag har lämnat kvar varje steg i koden, för att kunna gå tillbaka och kika på det igen. 
+   Det gör det tyvärr lite rörigt att läsa.
+2. Patch-metoden har jag försökt implementera på Tournament, men insåg att jag inte riktigt 
+   har koll på PATCH, så jag har kommit en bit på väg, hoppas jag, men inte hela vägen fram.
+ */
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TournamentAPIv2ApiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TournamentAPIv2ApiContext") ?? throw new InvalidOperationException("Connection string 'TournamentAPIv2ApiContext' not found.")));
@@ -20,8 +27,8 @@ builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
     .AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters();
 
-builder.Services.AddScoped<IGameRepository, GameRepository>();
-builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
+//builder.Services.AddScoped<IGameRepository, GameRepository>();
+//builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
 builder.Services.AddScoped<IUoW, UoW>();
 builder.Services.AddAutoMapper(typeof(TournamentMappings));
 
